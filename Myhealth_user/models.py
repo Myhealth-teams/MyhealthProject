305fbd50e7958084ab2a2120e54f5131e363285f
sakuraTnT <347726395@qq.com>
2019-11-19 21:22:58 +0800

    修改了用户模型

--- Myhealth_user/models.py
+++ Myhealth_user/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Users(models.Model):
     u_name = models.CharField(max_length=20,null=True)
     u_password = models.CharField(max_length=50)
-    u_tel = models.IntegerField(unique=True,null=False)
-    u_image = models.CharField(max_length=256,default=None)
+    u_tel = models.CharField(max_length=11,unique=True,null=False)
+    u_image = models.CharField(max_length=256,default=None,null=True)
 
     class Meta:
         db_table = "users"
