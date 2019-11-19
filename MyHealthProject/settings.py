94f089f454a63aecc5299a7944ff139bc0b7264d
ZJDTop <154469562@qq.com>
2019-11-19 19:21:13 +0800

    添加了admin模型ORM

--- MyHealthProject/settings.py
+++ MyHealthProject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Myhealth_user',
+    'Myhealth_admin',
 ]
 
 MIDDLEWARE = [
--- Myhealth_admin/models.py
+++ Myhealth_admin/models.py
@@ -7,7 +7,6 @@
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-
 class SysRole(models.Model):
     role_id = models.AutoField(primary_key=True)
     role_name = models.CharField(max_length=50)
